ba5ba7443429e06ad16a90b05d23048349c6780f
tomgreenfield <tomgreenfield@users.noreply.github.com>
2021-06-29 12:49:03 +0100

    Convert to JSX (#27)
    * Convert to JSX
    
    * Bump versions
    
    * Use props; code style amends
    
    * Switch to core image template
    
    * Remove redundant listener
    
    * Bump dependency
    
    * Update README

--- js/list-view.js
+++ js/list-view.js
@@ -1,86 +1,52 @@
-define([
-  'core/js/adapt',
-  'core/js/views/componentView'
-], function (Adapt, ComponentView) {
+import ComponentView from 'core/js/views/componentView';
 
-  var ListView = ComponentView.extend({
+class ListView extends ComponentView {
 
-    preRender: function () {
-      this.checkIfResetOnRevisit();
-      this.listenTo(Adapt, 'device:resize', this.resizeControl);
-    },
+  className() {
+    let classes = super.className();
 
-    postRender: function () {
-      this.setReadyStatus();
-
-      this.setupInviewCompletion('.component__widget');
-
-      this.setUpColumns();
-
-      if (!this.model.get('_animateList')) return;
-
-      this.$el.addClass('is-animated-list');
-      this.$('.list__container').on('onscreen.animate', this.checkIfOnScreen.bind(this));
-    },
-
-    resizeControl: function () {
-      this.setUpColumns();
-    },
-
-    setUpColumns: function () {
-      var columns = this.model.get('_columns');
-
-      if (columns < 2) return;
-
-      var isLarge = Adapt.device.screenSize === 'large';
-      this.$('.list__item').css('width', isLarge ? (100 / columns) + '%' : '');
-    },
-
-    checkIfResetOnRevisit: function () {
-      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
-
-      // If reset is enabled set defaults
-      if (isResetOnRevisit) {
-        this.model.reset(isResetOnRevisit);
-      }
-    },
-
-    /**
-     * Kicks off the list item animation once the list container is at least xx% on screen
-     */
-    checkIfOnScreen: function (event, measurements) {
-      const percentage = this.model.get('_percentInviewVertical') || 70;
-      if (measurements.percentInviewVertical < percentage) return;
-
-      $(event.currentTarget).addClass('is-inview').off('onscreen.animate');
-
-      this.animateListItems();
-    },
-
-    /**
-     * animates the list items in one-by-one
-     */
-    animateListItems: function () {
-      if (this.$el.hasClass('has-animated')) return;
-
-      this.$('.list__item').each(function (index, listItem) {
-        setTimeout(function () {
-          $(listItem).addClass('is-animating');
-        }, 200 * index);
-      });
-
-      this.$el.addClass('has-animated');
-    },
-
-    remove: function () {
-      this.$('.list__container').off('onscreen.animate');
-
-      ComponentView.prototype.remove.call(this);
+    if (this.model.get('_animateList')) {
+      classes += ' is-animated-list';
     }
-  }, {
-    template: 'list'
-  });
 
-  return ListView;
+    return classes;
+  }
 
-});
+  postRender() {
+    this.setReadyStatus();
+    this.setupInviewCompletion('.component__widget');
+
+    if (this.model.get('_animateList')) {
+      this.$('.list__container').on('onscreen.animate', this.checkIfOnScreen.bind(this));
+    }
+  }
+
+  /**
+   * Kicks off the list item animation once the list container is at least xx% on screen
+   */
+  checkIfOnScreen({ currentTarget }, { percentInviewVertical }) {
+    if (percentInviewVertical < this.model.get('_percentInviewVertical')) return;
+
+    $(currentTarget).off('onscreen.animate');
+    this.animateListItems();
+  }
+
+  /**
+   * animates the list items in one-by-one
+   */
+  animateListItems() {
+    this.model.getChildren().forEach((listItem, index) => {
+      setTimeout(listItem.toggleActive.bind(listItem), 200 * index);
+    });
+  }
+
+  remove() {
+    this.$('.list__container').off('onscreen.animate');
+
+    super.remove();
+  }
+};
+
+ListView.template = 'list.jsx';
+
+export default ListView;
---
+++ templates/list.jsx
@@ -0,0 +1,61 @@
+import Adapt from 'core/js/adapt';
+import React from 'react';
+import { templates, classes, html, compile } from 'core/js/reactHelpers';
+
+export default function List({ _columns, _orderedList, _items, ...props }) {
+  const hasColumns = _columns > 1;
+
+  return (
+    <div className="component__inner list__inner">
+      <templates.header {...props} />
+      <div className="component__widget list__widget">
+        <div
+          className={classes([
+            'list__container',
+            hasColumns && 'has-columns',
+            _orderedList && 'is-ordered-list'
+          ])}
+          role="list"
+        >
+          {_items.map(({ _isActive, _imageSrc, alt, title, body }, index) =>
+            <div
+              key={index}
+              className={classes([
+                'list__item',
+                _isActive && 'is-animating'
+              ])}
+              role="listitem"
+              style={(hasColumns && Adapt.device.screenSize === 'large' && { width: `${100 / _columns}%` }) || null}
+            >
+              <div className="list__item-inner">
+                {!_imageSrc ?
+                  <div className="list__item-bullet"></div> :
+                  <templates.image
+                    _src={_imageSrc}
+                    alt={alt}
+                    classNamePrefixes={[ 'list__item' ]}
+                  />
+                }
+                <div className="list__item-content">
+                  {title &&
+                    <div className={classes([
+                      'list__item-title',
+                      body && 'has-margin'
+                    ])}>
+                      <div className="list__item-title-inner">{html(compile(title))}</div>
+                    </div>
+                  }
+                  {body &&
+                    <div className="list__item-body">
+                      <div className="list__item-body-inner">{html(compile(body))}</div>
+                    </div>
+                  }
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
